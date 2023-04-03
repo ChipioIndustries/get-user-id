7a025a045975c558426e7e203641bfc84e9dda68
Chipio Industries <53354220+ChipioIndustries@users.noreply.github.com>
2023-04-03 14:08:40 -0400

    Fixed error when calling from the client

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.0.1
+
+* Fixed error when calling from the client.
+
 ## v1.0.0
 
 * Initial release.
--- src/init.lua
+++ src/init.lua
@@ -4,10 +4,10 @@ local RunService = game:GetService("RunService")
 local t = require(script.Parent.t)
 
 local function getUserId(player)
-	assert(t.instanceOf("Player")(player))
 	if not player and RunService:IsClient() then
 		player = Players.LocalPlayer
 	end
+	assert(instanceOf("Player")(player))
 	return tostring(player.UserId)
 end
 
